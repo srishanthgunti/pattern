4bbe2938a64c58034edf9f1f6ba00d5a77f6978c
srishanth <guntisrishanth403@gmail.com>
2024-12-09 14:51:46 +0530

    pattern.java

---
+++ pattern.java
@@ -0,0 +1,24 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int ii = n - 1; i > 0; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
