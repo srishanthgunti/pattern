550a052169113b2f5e70a25f849327726bc418de
srishanthgunti <guntisrishanth403@gmail.com>
2024-12-12 22:54:44 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,6 +1,7 @@
-public class DiamondPattern {
+public class Pattern {
     public static void main(String[] args) {
         int n = 5; // Number of rows
+        // Upper part of the diamond
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
@@ -10,15 +11,15 @@ public class DiamondPattern {
             }
             System.out.println();
         }
-        for (int ii = n - 1; i > 0; i--) {
-            for (int j = 1; j <= n - i; j++) {
+        // Lower part of the diamond
+        for (int ii = n - 1; ii > 0; ii--) { // Correct loop variable
+            for (int j = 1; j <= n - ii; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= (2 * i - 1); j++) {
+            for (int j = 1; j <= (2 * ii - 1); j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 }
-
